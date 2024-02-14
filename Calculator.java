86e0a3a5327221bb9bedbbb5af8e9a255a5a3747
Tbryant7 <tannonbryant@gmail.com>
2024-02-13 19:50:05 -0500

    Original with Addition and Subtraction

---
+++ Calculator.java
@@ -0,0 +1,33 @@
+/*Tannon Bryant
+ * 2/13/24
+ * Calculator that I commit over time
+*/
+import java.util.Scanner;
+
+
+
+
+public class Calculator{
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner( System.in);
+            System.out.println("Enter Two whole numbers.");
+                double x = scan.nextDouble();
+                double y = scan.nextDouble();
+                    scan.nextLine();
+
+        System.out.println("Would you like to do Addition or Subtraction");
+            String cmd = scan.nextLine();
+                double t;
+        switch(cmd.toLowerCase()) {
+            case "subtraction":
+                t = x - y;
+                System.out.println("Result:" + t);
+                    break;
+            case "addition":
+                t = x + y;
+                System.out.println("Result:" + t);
+                    break;
+        }
+    }
+}
